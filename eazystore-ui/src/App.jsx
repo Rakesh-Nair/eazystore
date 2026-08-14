b5e02ea82eefdea7090b3c182a0470e1737c4200
Rakesh Nair <crucifyrakesh@gmail.com>
2026-08-14 08:22:14 +0530

    Introduce Page Heading and Page Title Component

--- eazystore-ui/src/App.jsx
+++ eazystore-ui/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer/Footer";
+import Home from "./components/Home";
 import React from "react";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <Home />
       <Footer />
     </React.Fragment>
   );
