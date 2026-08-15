754598202f9ddc75be75523320ebcc47ca2f2343
Rakesh Nair <crucifyrakesh@gmail.com>
2026-08-15 20:13:23 +0530

    Add few npm modules

--- eazystore-ui/src/App.jsx
+++ eazystore-ui/src/App.jsx
@@ -4,6 +4,8 @@ import Header from "./components/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home";
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   return (
