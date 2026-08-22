a09e0474c0350e2f03a782520546c828307f72bd
Rakesh Nair <crucifyrakesh@gmail.com>
2026-08-22 18:32:44 +0530

    Add project to learn tailwind css

---
+++ tailwind-demo/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="text-lg underline font-bold">Tailwind makes styling easy !</p>
+    </>
+  );
+}
+
+export default App;
