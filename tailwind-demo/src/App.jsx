4ef5a47c88259ecdcf6f3cf69f5c9098164df370
Rakesh Nair <crucifyrakesh@gmail.com>
2026-08-22 19:32:41 +0530

    font changes

--- tailwind-demo/src/App.jsx
+++ tailwind-demo/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <p className="text-lg underline font-bold">Tailwind makes styling easy !</p>
+      <p className="text-lg underline font-bold mt-4 text-blue-600">Tailwind makes styling easy !</p>
     </>
   );
 }
