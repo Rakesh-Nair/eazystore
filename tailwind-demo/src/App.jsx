894736933f216e901453c28893b31a1c9fdf4d25
Rakesh Nair <crucifyrakesh@gmail.com>
2026-08-22 19:38:06 +0530

    Background demo

--- tailwind-demo/src/App.jsx
+++ tailwind-demo/src/App.jsx
@@ -4,7 +4,11 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <p className="text-lg underline font-bold mt-4 text-blue-600">Tailwind makes styling easy !</p>
+      <p className="text-lg underline font-bold mt-4 text-blue-600">
+        Tailwind makes styling easy !
+      </p>
+      <div className="bg-gray-200 p-4 m-4">Background Color Demo</div>
+      <div className="bg-green-300 border-green-600 p-4 m-4 rounded-lg">Border Demo</div>
     </>
   );
 }
