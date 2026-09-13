9e8efc32acbdcf507df6324723e00fe575173769
Rakesh Nair <crucifyrakesh@gmail.com>
2026-09-13 13:03:03 +0530

    Add sql and fix for h2 database

--- eazystore/src/test/java/com/eazybytes/eazystore/EazystoreApplicationTests.java
+++ eazystore/src/test/java/com/eazybytes/eazystore/EazystoreApplicationTests.java
@@ -1,13 +1,27 @@
 package com.eazybytes.eazystore;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class EazystoreApplicationTests {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void productsTableIsInitialized() {
+		Integer productCount = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM products", Integer.class);
+
+		Assertions.assertNotNull(productCount);
+		Assertions.assertTrue(productCount > 0);
 	}
 
 }
