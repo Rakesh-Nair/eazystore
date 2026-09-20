3eb80ea73c7a0add419fdb3ed5733d5df6866ed2
Rakesh Nair <crucifyrakesh@gmail.com>
2026-09-20 08:10:04 +0530

    Add logic for Error and Loading

--- eazystore-ui/src/components/Home.jsx
+++ eazystore-ui/src/components/Home.jsx
@@ -5,6 +5,8 @@ import { useState, useEffect } from "react";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   //Run once when the component mounts
 
   useEffect(() => {
@@ -12,11 +14,33 @@ export default function Home() {
   }, []);
 
   const fetchProducts = async () => {
-    const response = await apiClient.get("/products");
-
-    setProducts(response.data);
-    
+    try {
+      setLoading(true);
+      const response = await apiClient.get("/products");
+      setProducts(response.data);
+    } catch (error) {
+      setError(error.response?.data?.message || "Failed to fetch products, Please try again.");
+    } finally {
+      setLoading(false);
+    }
   };
+    if(loading) {
+      return (
+        <div className="flex items-center justify-center min-h-screen">
+          <span className="text-xl font-semibold">Loading Products...</span>
+        </div>
+      )
+
+    }
+
+    if(error){
+      return (
+        <div className="flex items-center justify-center min-h-screen">
+          <span className="text-xl font-semibold text-red-500">Error: {error}</span>
+        </div>
+      );
+    }
+  
   return (
     <div className="max-w-[1152px] mx-auto px-6 py-8">
       <PageHeading title="Explore Eazy Stickers!">
